6edc3882967a12902d9aea26ae3f7277fd1bec99
MonsterXiong <942849672@qq.com>
2024-02-21 22:16:22 +0800

    清楚菜单用于数据 && 跳转逻辑调整

--- src/layout/sideBar/menuData.js
+++ src/layout/sideBar/menuData.js
@@ -1,30 +1,5 @@
 import { routesConstant } from '@/router/routeConstant'
 
 export const menuList = [
-  {
-    id: '001',
-    parent: '',
-    code: 'design',
-    name: '设计',
-    icon: '',
-    menuType: 'module',
-  },
-  {
-    id: '001001',
-    parent: '001',
-    code: 'design_abi',
-    name: '能力清单',
-    icon: '',
-    menuType: 'page',
-    menuParams: routesConstant.DESIGN_ABI.path,
-  },
-  {
-    id: '001002',
-    parent: '001',
-    code: 'design_index',
-    name: '指标清单',
-    icon: '',
-    menuType: 'page',
-    menuParams: routesConstant.DESIGN_INDEX.path,
-  },
+
 ]
--- src/utils/navMenuUtil.js
+++ src/utils/navMenuUtil.js
@@ -9,11 +9,34 @@ export function isOpenNewWindow(url) {
  * 处理菜单点击事件
  * @param {*} menu 菜单项
  */
-export function handleNavMenuClick(menu) {
+export function handleNavMenuClick1(menu) {
   const { menuType, menuParams } = menu
   if (menuType == MENU_TYPE.PAGE) {
     if (router.currentRoute.fullPath != menuParams) {
       router.push(menuParams)
+    }
+  }
+}
+
+export function handleNavMenuClick(menu) {
+  let { menuType, menuParams } = menu
+  if (menuParams.includes('vue:')) {
+    menuParams = menuParams.split('vue:')[1]
+  } else if (menuParams.includes('static:')) {
+    // 外部链接则嵌套iframe
+    const url = encodeURIComponent(menuParams.split('static:')[1])
+    router.push(`${routesConstant.OTHER_PAGE.path}/${menu.id}?url=${url}`)
+    return
+  }
+  if (menuType == MENU_TYPE.PAGE && menuParams) {
+    let url = menuParams + `?projectId=${getCurrentProjectId()}&menuId=${menu.id}&tagName=${menu.name}`
+    const wordPagePath = [routesConstant.DESIGN_PAGE.path, routesConstant.READ_PAGE.path, routesConstant.FUNCTION_DESIGN_PAGE.path].join(',')
+    if (wordPagePath.includes(menuParams)) {
+      // url = menuParams + '/' + menu.id + `?tagName=${menu.name}`
+      url = menuParams + '/' + menu.id + `?projectId=${getCurrentProjectId()}&menuId=${menu.id}&tagName=${menu.name}`
+    }
+    if (router.currentRoute.fullPath != url) {
+      router.push(url)
     }
   }
 }
